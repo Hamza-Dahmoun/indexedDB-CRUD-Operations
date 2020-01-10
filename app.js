1c3beb2e3f68f633751af5a5b2dddd21a3b3a12a
Hamza <hamza.dahmoun7@gmail.com>
2020-01-10 22:13:30 +0100

    write & use createSampleData()

--- app.js
+++ app.js
@@ -4,18 +4,32 @@ document.addEventListener("DOMContentLoaded", function () {
     window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB
     if (!window.indexedDB) {
         //browser does not support indexedDB, lets inform user abt it
-        document.getElementById("welcome-screen").innerHTML = "Sorry! Your browser does not support IndexedDB!";
+        //document.getElementById("welcome-screen").innerHTML = "Sorry! Your browser does not support IndexedDB!";
+        displayWelcomeScreen("Sorry! Your browser does not support IndexedDB!", true);
     }
     else {
-        displayWelcomeScreen();
+        //lets create the DB with 2 tables and add 2 authors objects and 6 articles objects to it
+        createSampleData();
     }
 });
-
+function createSampleData(){
+    //this function creates and connect to the database, and stores 2 authors objects and 6 articles objects to the database
+    var db; //db is going to be linked to the indexedDB we use
+    let openRequest = indexedDB.open("myDatabase");
+    openRequest.onsuccess = function(event){
+        //assignation of myDatabase to 'db' variable
+        db = event.target.result;
+        displayWelcomeScreen();
+    };
+    openRequest.onerror = function(event){
+        displayWelcomeScreen("Something went wrong when trying to open the database... error code: " + event.target.errorCode, true);
+    }
+}
 function displayWelcomeScreen(msg, isError) {
     let welcomeMsg = document.getElementById("welcome-screen").querySelector("#welcome-msg");
     let errorMsg = document.getElementById("welcome-screen").querySelector("#error-msg");
     if(isError){        
-        errorMsg.innerHTML = "Sorry! Your browser does not support IndexedDB!";
+        errorMsg.innerHTML = msg;
         errorMsg.style.display = "block";
         welcomeMsg.style.display = "none";
     }
