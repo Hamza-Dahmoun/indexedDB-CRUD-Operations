e8eb6e9191bd81b2d306a10efe7684210f06d458
Hamza <hamza.dahmoun7@gmail.com>
2020-01-14 19:56:56 +0100

    add click events for nav in small screens

--- app.js
+++ app.js
@@ -392,3 +392,30 @@ function getAuthorByID_Promise(authorID) {
     });
 }
 
+
+
+
+/********************************************* MENU BUTTON CLICKS IN MOBILE DEVICE */
+document.querySelector("#navbar i").addEventListener("click", showMenu);
+function showMenu() {
+    let menuBtnContainer = document.getElementById("menu-buttons-container");
+    menuBtnContainer.className = "menu-buttons-container-shown";
+    /*Please read the description of css class .menu-buttons-container-shown in css file*/
+
+
+    document.querySelector("#navbar i").removeEventListener("click", showMenu);
+    document.querySelector("#navbar i").addEventListener("click", hideMenu);
+}
+function hideMenu() {
+    let menuBtnContainer = document.getElementById("menu-buttons-container");
+    menuBtnContainer.className = "menu-buttons-container-hidden";
+    /*Please read the description of css class .menu-buttons-container-hidden in css file*/
+
+    document.querySelector("#navbar i").removeEventListener("click", hideMenu);
+    document.querySelector("#navbar i").addEventListener("click", showMenu);
+}
+let menuLinks = document.querySelectorAll("#navbar a");
+for(let i=0; i<menuLinks.length; i++){
+    menuLinks[i].addEventListener("click", hideMenu);
+}
+
